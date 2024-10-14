02408e6d1338bf75e0b17cd6085f8e5eae99f6d3
Yacine-df <ycn.diaf@gmail.com>
2024-10-14 22:15:40 +0300

    create project model and migration, set up relation with team

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -45,4 +45,9 @@ class Team extends JetstreamTeam
             'personal_team' => 'boolean',
         ];
     }
+
+    public function Projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }

a287337288ffc4d25b4fd0e1a57cf9e4294d743c
Yacine-df <ycn.diaf@gmail.com>
2024-11-18 20:29:12 +0000

    Create relationship between the project and the task

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Project extends Model
@@ -30,5 +31,10 @@ class Project extends Model
     public function members()
     {
         return $this->team->users();
+    }
+
+    public function tasks(): HasMany
+    {
+         return $this->hasMany(Task::class);
     }
 }

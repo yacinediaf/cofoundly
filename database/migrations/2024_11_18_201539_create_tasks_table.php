ae76fa63c4b21eb51424d0edbdc70455524052f2
Yacine-df <ycn.diaf@gmail.com>
2024-11-18 20:25:56 +0000

    Create task model and migration

---
+++ database/migrations/2024_11_18_201539_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Project;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Project::class)->cascadeOndelete();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('assigned_to')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};

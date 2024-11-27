ef8b421e72b09d4c80dc9a02beb50e2eefd14ebb
Yacine-df <ycn.diaf@gmail.com>
2024-11-27 20:43:37 +0000

    Creating the task form

--- resources/js/Pages/Tasks/Create.vue
+++ resources/js/Pages/Tasks/Create.vue
@@ -4,6 +4,8 @@ import { usePage } from '@inertiajs/vue3';
 import { computed, ref } from 'vue';
 import { Calendar } from '@/Components/ui/calendar'
 import { getLocalTimeZone, today } from '@internationalized/date'
+import Label from '@/Components/ui/label/Label.vue';
+import Input from '@/Components/ui/input/Input.vue';
 
 defineOptions({
     layout: AppLayout
@@ -14,6 +16,7 @@ defineProps(['members']);
 let userName = ($member) => computed(() => usePage().props.auth.user.id == $member.id ? 'Me' : $member.name)
 let selectedMember = ref(null);
 const value = ref(today(getLocalTimeZone()))
+
 const assignTo = ($member) => {
     selectedMember.value = $member.id
 }
@@ -21,14 +24,14 @@ const assignTo = ($member) => {
 <template>
     <div class="h-full">
         <div class="h-full p-6 lg:p-8 bg-white border-b border-gray-200">
-            <div class="w-full flex">
+            <div class="w-full flex gap-10">
                 <div class="w-64 flex flex-col gap-8">
                     <div class="space-y-4">
                         <div>
                             <h1 class="font-semibold text-2xl mb-2">Team Members</h1>
                             <p class="text-xs text-gray-500">Assign the task to one of the following users.</p>
                         </div>
-                        <div class="flex gap-3 items-center">
+                        <div class="flex flex-wrap gap-3 items-center">
                             <div v-for="member in members" :key="member.id" @click="assignTo(member)"
                                 class="flex flex-col gap-1 items-center cursor-pointer">
                                 <img class="h-14 w-14 shadow-md rounded-full transition-all ease-in-out"
@@ -52,7 +55,18 @@ const assignTo = ($member) => {
                         <Calendar v-model="value" :weekday-format="'short'" class="rounded-md border" />
                     </div>
                 </div>
-                <div>form</div>
+                <section>
+                    <h1 class="font-semibold text-2xl mb-2">What's Up, New Task ? Let's go 🤩💻</h1>
+
+                    <div class="mt-5">
+                        <form>
+                            <div>
+                                <Label>Task Title</Label>
+                                <Input class="mt-2" />
+                            </div>
+                        </form>
+                    </div>
+                </section>
             </div>
         </div>
     </div>

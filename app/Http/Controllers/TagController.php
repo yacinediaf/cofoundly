de55f1d62438e823d596bbe0958b60776c5fb37f
yacinediaf <ycn.diaf@gmail.com>
2025-01-16 20:43:24 +0000

    Add `update and destroy` logics

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class TagController extends Controller
 {
     public function store(Request $request, Project $project)
     {
         $attributes = $request->validate([
-            'name' => 'required|unique:tags|max:255',
+            'name' => ['required', 'max:255', 'unique:tags'],
             'color' => 'required'
         ]);
 
@@ -18,5 +20,26 @@ class TagController extends Controller
 
         return redirect(route('projects.show', $project))
                 ->with('success', 'Tag created successfully.');
+    }
+
+    public function update(Request $request, Tag $tag)
+    {
+        $attributes = $request->validate([
+            'name' => ['required', 'max:255', Rule::unique('tags')->ignore($tag)],
+            'color' => 'required'
+        ]);
+
+        $tag->update($attributes);
+
+        return redirect(route('projects.show', $tag->project))
+                ->with('success', 'Tag updated successfully.');
+    }
+
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+
+        return redirect(route('projects.show', $tag->project))
+                ->with('success', 'Tag deleted successfully.');
     }
 }

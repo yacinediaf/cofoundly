0c63b72b06f7feff0d2d50c29f6ecbf5b0b1086c
Yacine Diaf <ycn.diaf@gmail.com>
2025-10-20 00:44:33 +0000

    fix: typo

--- resources/js/Components/ui/calendar/CalendarCellTrigger.vue
+++ resources/js/Components/ui/calendar/CalendarCellTrigger.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/Components/ui/button';
 import { cn } from '@/lib/utils';
 import { CalendarCellTrigger, useForwardProps } from 'radix-vue';
 import { computed } from 'vue';
@@ -22,25 +22,21 @@ const forwardedProps = useForwardProps(delegatedProps);
 </script>
 
 <template>
-  <CalendarCellTrigger
-    :class="
-      cn(
-        buttonVariants({ variant: 'ghost' }),
-        'h-8 w-8 p-0 font-normal',
-        '[&[data-today]:not([data-selected])]:bg-accent [&[data-today]:not([data-selected])]:text-accent-foreground',
-        // Selected
-        'data-[selected]:bg-primary data-[selected]:text-primary-foreground data-[selected]:opacity-100 data-[selected]:hover:bg-primary data-[selected]:hover:text-primary-foreground data-[selected]:focus:bg-primary data-[selected]:focus:text-primary-foreground',
-        // Disabled
-        'data-[disabled]:text-muted-foreground data-[disabled]:opacity-50',
-        // Unavailable
-        'data-[unavailable]:text-destructive-foreground data-[unavailable]:line-through',
-        // Outside months
-        'data-[outside-view]:text-muted-foreground data-[outside-view]:opacity-50 [&[data-outside-view][data-selected]]:bg-accent/50 [&[data-outside-view][data-selected]]:text-muted-foreground [&[data-outside-view][data-selected]]:opacity-30',
-        props.class,
-      )
-    "
-    v-bind="forwardedProps"
-  >
+  <CalendarCellTrigger :class="cn(
+    buttonVariants({ variant: 'ghost' }),
+    'h-8 w-8 p-0 font-normal',
+    '[&[data-today]:not([data-selected])]:bg-accent [&[data-today]:not([data-selected])]:text-accent-foreground',
+    // Selected
+    'data-[selected]:bg-primary data-[selected]:text-primary-foreground data-[selected]:opacity-100 data-[selected]:hover:bg-primary data-[selected]:hover:text-primary-foreground data-[selected]:focus:bg-primary data-[selected]:focus:text-primary-foreground',
+    // Disabled
+    'data-[disabled]:text-muted-foreground data-[disabled]:opacity-50',
+    // Unavailable
+    'data-[unavailable]:text-destructive-foreground data-[unavailable]:line-through',
+    // Outside months
+    'data-[outside-view]:text-muted-foreground data-[outside-view]:opacity-50 [&[data-outside-view][data-selected]]:bg-accent/50 [&[data-outside-view][data-selected]]:text-muted-foreground [&[data-outside-view][data-selected]]:opacity-30',
+    props.class,
+  )
+    " v-bind="forwardedProps">
     <slot />
   </CalendarCellTrigger>
 </template>

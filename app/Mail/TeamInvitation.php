b3ca5402993a1b986b49e6254c2770756332c89e
Yacine Diaf <ycn.diaf@gmail.com>
2025-10-24 23:54:07 +0000

    fix: the mail bug

---
+++ app/Mail/TeamInvitation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Startup;
+use App\Models\TeamInvitation as TeamInvitationModel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\URL;
+
+class TeamInvitation extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public TeamInvitationModel $invitation, public Startup $startup)
+    {
+        //
+    }
+
+    /**
+        * Build the message.
+        *
+        * @return $this
+        */
+    public function build()
+    {
+        return $this->markdown('emails.team-invitation', ['acceptUrl' => URL::signedRoute('team-invitations.accept', [
+            'invitation' => $this->invitation,
+        ])])->subject(__('Team Invitation'));
+    }
+}
